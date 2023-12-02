dd1536e22c58049073c5dac3d56dc5237c9f7fb0
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2023-12-02 18:36:36 +0330

    Vasate 11 Azar

--- bank/main.py
+++ bank/main.py
@@ -62,6 +62,33 @@ def withdraw():
     messagebox.showinfo("Success", f"Get your {amount}$")
     save('bank/'+data["card number"][0:4]+".json", data)
 
+def send():
+    global data
+    amount = float(entry_amount.get())
+    to = entry_destination.get()
+    answer = messagebox.askyesno("Sure?", f"Are you Siroos you want to transfer {amount}$ to {to}?")
+    if answer:
+        balance = data['balance']
+        if amount<0:
+            messagebox.showerror("Titr", "Don't NIGGA.")
+            return
+        if balance-amount<0:
+            messagebox.showerror("Titr", "Don't Money.")
+            return
+        try:
+            f = open(f"bank/{to}.json")
+            balance -= amount
+            data['balance'] = balance
+            save('bank/'+data["card number"][0:4]+".json", data)
+            new_data = load(f)
+            new_data['balance']+=amount
+            save(f"bank/{to}.json", new_data)
+        except FileNotFoundError:
+            messagebox.showerror("Titr", "Card is Cake.")
+
+        
+
+
 config_btn = {
     'bg': 'pink',
     'fg': 'purple',
@@ -105,7 +132,8 @@ transfer_window.withdraw()
 withdraw_window.withdraw()
 btn_withdraw   = Button(manage_window, text='Withdraw', cnf=config_btn,
 command=lambda:change_window(withdraw_window, manage_window))
-btn_transfer   = Button(manage_window, text='Transfer', cnf=config_btn)
+btn_transfer   = Button(manage_window, text='Transfer', cnf=config_btn,
+command=lambda:change_window(transfer_window, manage_window))
 btn_balance    = Button(manage_window, text='Balance', cnf=config_btn,
                                 command=balance)
 btn_change_pin = Button(manage_window, text='Change Pin', cnf=config_btn)
@@ -114,6 +142,18 @@ btn_transfer.grid  (row=1, column=1, sticky='news', padx=20, pady=10)
 btn_balance.grid   (row=2, column=1, sticky='news', padx=20, pady=10)
 btn_change_pin.grid(row=2, column=2, sticky='news', padx=20, pady=10)
 
+label_amount                = Label(transfer_window, text='Give Money: ', cnf=config_btn)
+label_destination           = Label(transfer_window, text='To Who?', cnf=config_btn)
+entry_amount                = Entry(transfer_window, cnf=config_entry)
+entry_destination           = Entry(transfer_window, cnf=config_entry)
+btn_send                    = Button(transfer_window, cnf=config_btn, text="Send", command=send)
+btn_back_transfer_window    = Button(transfer_window, cnf=config_btn, text="Back", command=lambda:change_window(manage_window, transfer_window))
+label_amount                .grid(row=1, column=1)
+label_destination           .grid(row=2, column=1)
+entry_amount                .grid(row=1, column=2)
+entry_destination           .grid(row=2, column=2)
+btn_send                    .grid(row=3, column=1)
+btn_back_transfer_window    .grid(row=3, column=2)
 
 e_pin.pack(padx=15, pady=10)
 btn_enter.pack(padx=15, pady=10)
